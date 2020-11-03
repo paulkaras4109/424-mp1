ec15940aee089a030f3216801acc0c806862542e
Paul Karas <pakaras2@illinois.edu>
2020-11-03 16:08:43 -0600

    bugfix

--- YOLOv3/process_frame.py
+++ YOLOv3/process_frame.py
@@ -1,38 +1,49 @@
 from scheduling.misc import *
 from scheduling.TaskEntity import *
-
+import numpy as np
 
 # read the input bounding box data from file
 box_info = read_json_file('../dataset/waymo_ground_truth_flat.json')
-
+recent_boxes = []
+frame_num = 1
+threshold = 25
 
 def process_frame(frame):
     """Process frame for scheduling.
-
     Process a image frame to obtain cluster boxes and corresponding scheduling parameters
     for scheduling. 
-
     Student's code here.
-
     Args:
         param1: The image frame to be processed. 
-
     Returns:
         A list of tasks with each task containing image_path and other necessary information. 
     """
+    global frame_num
+    global recent_boxes
+    for rb in recent_boxes:
+        if (frame_num - rb[1] > 9):
+            recent_boxes.remove(rb)
 
     tasklist = []
-    #cluster_boxes_data = get_bbox_info(frame, box_info)
     # student's code here
-    #box_info = read_json_file(frame.path)
     cluster_boxes_data = get_bbox_info(frame, box_info)
     for bbox in cluster_boxes_data:
-        priority = int(bbox[4]/10)
-        task = TaskEntity(frame.path, coord = bbox[:4], priority = priority, depth = bbox[4])
-        tasklist.append(task)
+        simFound = False
+        for rb in recent_boxes:
+            if (abs(bbox[0] - rb[0][0]) < threshold and 
+            abs(bbox[1] - rb[0][1]) < threshold and 
+            abs(bbox[2] - rb[0][2]) < threshold and 
+            abs(bbox[3] - rb[0][3]) < threshold):
+                rb = (bbox[:4], frame_num)
+                simFound = True
+                break
+        
+        if not simFound:
+            priority = int(bbox[4]/10)
+            task = TaskEntity(frame.path, coord = bbox[:4], priority = priority, depth = bbox[4])
+            tasklist.append(task)
+            tp = (bbox[:4], frame_num)
+            recent_boxes.append(tp)
     
-
+    frame_num += 1
     return tasklist
-
-
-    
