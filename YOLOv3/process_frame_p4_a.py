abcba43162bc72d113425959d7def0834fc97e89
jconnor3317 <35938414+jconnor3317@users.noreply.github.com>
2020-11-03 15:45:34 -0600

    Update process_frame_p4_a.py

--- YOLOv3/process_frame_p4_a.py
+++ YOLOv3/process_frame_p4_a.py
@@ -6,25 +6,23 @@ import numpy as np
 box_info = read_json_file('../dataset/waymo_ground_truth_flat.json')
 recent_boxes = []
 frame_num = 1
+threshold = 25
 
 def process_frame(frame):
     """Process frame for scheduling.
-
     Process a image frame to obtain cluster boxes and corresponding scheduling parameters
     for scheduling. 
-
     Student's code here.
-
     Args:
         param1: The image frame to be processed. 
-
     Returns:
         A list of tasks with each task containing image_path and other necessary information. 
     """
     global frame_num
     global recent_boxes
-    if (frame_num % 10 == 0):
-        recent_boxes = []
+    for rb in recent_boxes:
+        if (frame_num - rb[1] > 9):
+            recent_boxes.remove(rb)
 
     tasklist = []
     # student's code here
@@ -32,12 +30,11 @@ def process_frame(frame):
     for bbox in cluster_boxes_data:
         simFound = False
         for rb in recent_boxes:
-            if (rb[0] > 0 and rb[1] > 0 and rb[2] > 0 and rb[3] > 0 and 
-            abs((bbox[:4][0] - rb[0]) / rb[0]) < 0.05 and 
-            abs((bbox[:4][1] - rb[1]) / rb[1]) < 0.05 and 
-            abs((bbox[:4][2] - rb[2]) / rb[2]) < 0.05 and 
-            abs((bbox[:4][3] - rb[3]) / rb[3]) < 0.05):
-                rb = bbox[:4]
+            if (abs(bbox[0] - rb[0]) < threshold and 
+            abs(bbox[1] - rb[1]) < threshold and 
+            abs(bbox[2] - rb[2]) < threshold and 
+            abs(bbox[3] - rb[3]) < threshold):
+                rb = tuple(bbox[:4], frame_num)
                 simFound = True
                 break
         
@@ -49,6 +46,3 @@ def process_frame(frame):
     
     frame_num += 1
     return tasklist
-
-
-    
